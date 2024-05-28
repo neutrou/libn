5d991cc1592abec6c667002384ff50659a14e0f8
neutroufromfortytwo <itsneutrouthetrou@gmail.com>
2024-05-28 10:37:53 +0200

    norm

--- cpydchar.c
+++ cpydchar.c
@@ -1,7 +1,18 @@
-#include <stdlib.h>
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cpydchar.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: valgrant <valgrant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/27 19:00:02 by valgrant          #+#    #+#             */
+/*   Updated: 2024/05/27 19:00:16 by valgrant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char	**copydchar(char** og)
+#include "libn.h"
+
+char	**copydchar(char **og)
 {
 	int		i;
 	int		nrow;
@@ -12,11 +23,11 @@ char	**copydchar(char** og)
 	nrow = 0;
 	while (og[nrow] != NULL)
 		nrow++;
-	copy = (char**)malloc((nrow + 1) * sizeof(char *));
+	copy = (char **)malloc((nrow + 1) * sizeof(char *));
 	while (og[++i] != NULL)
 	{
 		len = strlen(og[i]);
-		copy[i] = (char*)malloc((len + 1) * sizeof(char));
+		copy[i] = (char *)malloc((len + 1) * sizeof(char));
 		ft_strlcpy(copy[i], og[i], len);
 	}
 	copy[nrow] = NULL;
